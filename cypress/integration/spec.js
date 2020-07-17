38d08ef3166df4119fa2271ea3bd76991b078894
Nathan Phillips <nathanphillips@MacBook-Pro-7.broadband>
2020-07-17 11:50:02 +0100

    Example of slow down with toggling window

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,15 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+	it('empty test case defaults main test to closed', () => {
+		// try deleting this test and see how much slow the lower test runs
+	})
+
+	it('works', () => {
+		cy.visit('https://example.cypress.io/commands/actions')
+		// takes about a minute
+		for (let i = 0; i < 200; i++) {
+				cy.get('.action-email')
+					.type(`input${i}`).should('have.value', 'input' + i).clear()
+		}
+	})
 })
